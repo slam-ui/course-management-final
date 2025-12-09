28011e7685326a6fbaac23f2a680748ec957a984
La_Free <c4p0ker@yandex.ru>
2025-12-09 14:32:59 +0300

    Remove unused import in IndexController
    Removed unused import for PluginLoader service.

--- src/main/java/ru/mtuci/coursemanagement/controller/IndexController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/IndexController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.mtuci.coursemanagement.service.PluginLoader;
 
 @Controller
 @RequiredArgsConstructor
