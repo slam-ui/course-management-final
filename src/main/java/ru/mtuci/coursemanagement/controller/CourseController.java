a992d0d7b7c6668a075954e5830349bdc3736875
La_Free <c4p0ker@yandex.ru>
2025-12-09 14:23:45 +0300

    Add PasswordEncoder import to CourseController

--- src/main/java/ru/mtuci/coursemanagement/controller/CourseController.java
+++ src/main/java/ru/mtuci/coursemanagement/controller/CourseController.java
@@ -1,5 +1,6 @@
 package ru.mtuci.coursemanagement.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
