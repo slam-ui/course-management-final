772368ddedd2bd2d6e524e3585732d1adcd863c2
La_Free <c4p0ker@yandex.ru>
2025-12-09 13:52:35 +0300

    Refactor searchByTitle method to use parameterized query

--- src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
+++ src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
@@ -32,14 +32,13 @@ public class CourseService {
     }
 
     public List<Course> searchByTitle(String title) {
-        String sql = "SELECT id, title, description, teacher_id FROM courses WHERE title = '" + title + "'";
-        RowMapper<Course> rm = (rs, i) -> new Course(
-                rs.getLong("id"),
-                rs.getString("title"),
-                rs.getString("description"),
-                rs.getLong("teacher_id")
-        );
-        return jdbc.query(sql, rm);
-
-    }
+    String sql = "SELECT id, title, description, teacher_id FROM courses WHERE title = ?";
+    RowMapper<Course> rm = (rs, i) -> new Course(
+            rs.getLong("id"),
+            rs.getString("title"),
+            rs.getString("description"),
+            rs.getLong("teacher_id")
+    );
+    return jdbc.query(sql, rm, title);
 }
+
