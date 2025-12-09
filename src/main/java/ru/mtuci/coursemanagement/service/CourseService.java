f856c43c9c441ac7729fba04f2e28b335937a03b
La_Free <c4p0ker@yandex.ru>
2025-12-09 14:07:09 +0300

    Fix indentation in searchByTitle method

--- src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
+++ src/main/java/ru/mtuci/coursemanagement/service/CourseService.java
@@ -32,13 +32,13 @@ public class CourseService {
     }
 
     public List<Course> searchByTitle(String title) {
-    String sql = "SELECT id, title, description, teacher_id FROM courses WHERE title = ?";
-    RowMapper<Course> rm = (rs, i) -> new Course(
-            rs.getLong("id"),
-            rs.getString("title"),
-            rs.getString("description"),
-            rs.getLong("teacher_id")
-    );
-    return jdbc.query(sql, rm, title);
-}
-
+        String sql = "SELECT id, title, description, teacher_id FROM courses WHERE title = ?";
+        RowMapper<Course> rm = (rs, i) -> new Course(
+                rs.getLong("id"),
+                rs.getString("title"),
+                rs.getString("description"),
+                rs.getLong("teacher_id")
+        );
+        return jdbc.query(sql, rm, title);
+    }
+} 
